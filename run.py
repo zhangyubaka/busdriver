9c9cc88d713af285c23fc4d86e8d465ba11b59d1
zhangyubaka <zhangyubaka@gmail.com>
2016-01-17 14:10:20 -0600

    Del some useless stuff and add a ref to Budda code

--- run.py
+++ run.py
@@ -11,14 +11,12 @@ def main():
 
     data_type = input('Input your code type eg:Morse \n')
 
-    if data_type == 'morse':
+    if data_type == 'morse' or data_type == 'Morse' or data_type == ‘摩丝’:
         msg = input('Type msg \n')
         r = morse.decode_morse(msg)
         print (r)
-    elif data_type == 'Morse':
-        msg = input('Type msg \n')
-        r = morse.decode_morse(msg)
-        print (r)
+    elif data_type == 'budda' or data_type == '佛经' or data_type == 'Budda':
+        print ("Please go to http://www.keyfc.net/bbs/tools/tudoucode.aspx")
     else:
         print("We don't support other type yet \n")
 
