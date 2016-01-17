7f31e74bf367eb8e38a339c3d8b116f8cc2ff3ed
zhangyubaka <zhangyubaka@gmail.com>
2016-01-17 13:48:20 -0600

    Add a help

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ import re
 from codec import morse
 import sys
 
+if "-h" in sys.argv or "--help" in sys.argv or len(sys.argv) < 2:
+    print ("Just run this program")
+    exit(-1)
+
 def main():
 
     data_type = input('Input your code type eg:Morse \n')
